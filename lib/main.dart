3683862e8f76f20b585a14c4d05c182d9444b678
Marcos César Medeiros <marcolla.hc@gmail.com>
2023-11-04 21:22:20 -0300

    feat: add firebase configurations files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:procuracaoapp/views/view_login.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
