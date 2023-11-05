6a0e30a3daa65977bf8c6bf1ce73814de97ce046
Marcos Medeiros <Marcos Csar Medeiros>
2023-11-05 14:46:37 -0300

    feat: ensureInitialized() to run app in Android

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:procuracaoapp/views/view_login.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
