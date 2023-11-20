f29cf79d1e5706972006f06d6804565fc0098696
Marcos Medeiros <Marcos Csar Medeiros>
2023-11-20 00:00:54 -0300

    feat: post and comment controllers

---
+++ lib/bloc/comment_bloc.dart
@@ -0,0 +1,137 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:procuracaoapp/model/comment_model.dart';
+
+class CommentBloc extends Bloc<CommentEvent, CommentState> {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  String? commentId;
+  String? uid;
+
+  CommentBloc() : super(WithoutComments()) {
+    on<CreateComment>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('comments').add(
+            {
+              'uid': event.comment.uid,
+              'postModel': event.comment.postModel.uid,
+              'content': event.comment.content,
+              'coordenates': event.comment.coordenates,
+              'read': event.comment.read,
+            },
+          );
+        } catch (e) {
+          emit(ErrorComments(
+              message:
+                  'Não foi possível cadastrar o comentário, tente novamente.'));
+        }
+      },
+    );
+
+    on<RetrieveComment>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('comments').get();
+        } catch (e) {
+          emit(ErrorComments(
+              message:
+                  'Não foi possível obter os contários, tente novamente.'));
+        }
+      },
+    );
+
+    on<RetrieveOneComment>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('comments').doc(event.commentId).get();
+        } catch (e) {
+          emit(ErrorComments(
+              message:
+                  'Não foi possível obter esse comentário, tente novamente.'));
+        }
+      },
+    );
+
+    on<UpdateComment>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('comments').doc(event.commentId).update(
+            {
+              'uid': event.comment.uid,
+              'postModel': event.comment.postModel.uid,
+              'content': event.comment.content,
+              'coordenates': event.comment.coordenates,
+              'read': event.comment.read,
+            },
+          );
+        } catch (e) {
+          emit(ErrorComments(
+              message:
+                  'Não foi possível atualizar o comentário, tente novamente.'));
+        }
+      },
+    );
+
+    on<DeleteComment>(
+      (event, emit) {
+        try {
+          firestore
+              .doc(uid)
+              .collection('comments')
+              .doc(event.commentId)
+              .delete();
+        } catch (e) {
+          emit(ErrorComments(
+              message:
+                  'Não foi possível remover o comentário, tente novamente.'));
+        }
+      },
+    );
+  }
+}
+
+abstract class CommentEvent {}
+
+class CreateComment extends CommentEvent {
+  CommentModel comment;
+
+  CreateComment({required this.comment});
+}
+
+class RetrieveComment extends CommentEvent {}
+
+class RetrieveOneComment extends CommentEvent {
+  String commentId;
+
+  RetrieveOneComment({required this.commentId});
+}
+
+class UpdateComment extends CommentEvent {
+  String commentId;
+  CommentModel comment;
+
+  UpdateComment({required this.commentId, required this.comment});
+}
+
+class DeleteComment extends CommentEvent {
+  String commentId;
+
+  DeleteComment({required this.commentId});
+}
+
+abstract class CommentState {}
+
+class WithoutComments extends CommentState {}
+
+class ObtainedComments extends CommentState {
+  CommentModel comment;
+
+  ObtainedComments({required this.comment});
+}
+
+class ErrorComments extends CommentState {
+  final String message;
+
+  ErrorComments({required this.message});
+}
--- lib/bloc/post_bloc.dart
+++ lib/bloc/post_bloc.dart
@@ -5,15 +5,86 @@ import 'package:procuracaoapp/model/post_model.dart';
 class PostBloc extends Bloc<PostEvent, PostState> {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  String? postId;
   String? uid;
 
   PostBloc() : super(WithoutPosts()) {
-    firestore
-      .doc(uid)
-      .collection("posts")
-      .add({
-        
-      })
+    on<CreatePost>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('posts').add(
+            {
+              'uid': event.post.uid,
+              'userModel': event.post.userModel.uid,
+              'name': event.post.name,
+              'description': event.post.description,
+              'photo': event.post.photo,
+              'coordenates': event.post.coordenates,
+            },
+          );
+        } catch (e) {
+          emit(ErrorPosts(
+              message:
+                  'Não foi possível cadastrar a postagem, tente novamente.'));
+        }
+      },
+    );
+
+    on<RetrievePost>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('posts').get();
+        } catch (e) {
+          emit(ErrorPosts(
+              message: 'Não foi possível obter postagens, tente novamente.'));
+        }
+      },
+    );
+
+    on<RetrieveOnePost>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('posts').doc(event.postId).get();
+        } catch (e) {
+          emit(ErrorPosts(
+              message:
+                  'Não foi possível obter essa postagem, tente novamente.'));
+        }
+      },
+    );
+
+    on<UpdatePost>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('posts').doc(event.postId).update(
+            {
+              'uid': event.post.uid,
+              'userModel': event.post.userModel.uid,
+              'name': event.post.name,
+              'description': event.post.description,
+              'photo': event.post.photo,
+              'coordenates': event.post.coordenates,
+            },
+          );
+        } catch (e) {
+          emit(ErrorPosts(
+              message:
+                  'Não foi possível atualizar a postagem, tente novamente.'));
+        }
+      },
+    );
+
+    on<DeletePost>(
+      (event, emit) {
+        try {
+          firestore.doc(uid).collection('posts').doc(event.postId).delete();
+        } catch (e) {
+          emit(ErrorPosts(
+              message:
+                  'Não foi possível remover a postagem, tente novamente.'));
+        }
+      },
+    );
   }
 }
 
@@ -22,14 +93,29 @@ abstract class PostEvent {}
 class CreatePost extends PostEvent {
   PostModel post;
 
-  Add(post);
+  CreatePost({required this.post});
 }
 
 class RetrievePost extends PostEvent {}
 
-class UpdatePost extends PostEvent {}
+class RetrieveOnePost extends PostEvent {
+  String postId;
 
-class DeletePost extends PostEvent {}
+  RetrieveOnePost({required this.postId});
+}
+
+class UpdatePost extends PostEvent {
+  String postId;
+  PostModel post;
+
+  UpdatePost({required this.postId, required this.post});
+}
+
+class DeletePost extends PostEvent {
+  String postId;
+
+  DeletePost({required this.postId});
+}
 
 abstract class PostState {}
 
