a57246ac0401be2becfb3b64449a5346556f3bbf
GutoRomagnolo <guromagnolo23@hotmail.com>
2023-12-06 00:24:55 -0300

    feat: adding post details component

--- lib/views/restrict_area/view_posts.dart
+++ lib/views/restrict_area/view_posts.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:procuracaoapp/components/post_component.dart';
 import 'package:procuracaoapp/mock.dart';
+import 'package:procuracaoapp/model/post_model.dart';
+import './../../components/post_details_components.dart'; // Importe o arquivo que contém os detalhes do post
 
 class ViewPosts extends StatelessWidget {
   const ViewPosts({super.key});
@@ -12,7 +14,23 @@ class ViewPosts extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: ListView(
         scrollDirection: Axis.vertical,
-        children: postsMock.map((e) => PostComponent(postModel: e)).toList(),
+        children: postsMock.map((e) {
+          return InkWell(
+            onTap: () {
+              _openPostDetails(context, e);
+            },
+            child: PostComponent(postModel: e),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  void _openPostDetails(BuildContext context, PostModel post) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PostDetailsScreen(postModel: post),
       ),
     );
   }
